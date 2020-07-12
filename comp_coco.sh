575661d72ac8e34f6da797321331907a2d4f50d4
Zhiyi Lai <zhiyilai@gypsum.cm.cluster>
2020-07-12 01:20:50 -0400

    before merge new feat

--- comp_coco.sh
+++ comp_coco.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=comp_detr
 #SBATCH --output=res_%j.txt  # output file
 #SBATCH -e res_%j.err        # File to which STDERR will be written
-#SBATCH --partition=m40-long # Partition to submit to
+#SBATCH --partition=2080ti-long # Partition to submit to
 #
 #SBATCH --ntasks=1
 #SBATCH --time=00-12:00       # Runtime in D-HH:MM
@@ -21,4 +21,4 @@ python -m torch.distributed.launch --use_env main.py --no_aux_loss --eval \
     --batch_size 1 --dilation \
     --resume https://dl.fbaipublicfiles.com/detr/detr-r101-dc5-a2e86def.pth \
     --coco_path /mnt/nfs/scratch1/zhiyilai/coco \
-    --output_dir /mnt/nfs/scratch1/zhiyilai/comp/detr/r101-dc5
+    --output_dir /mnt/nfs/scratch1/zhiyilai/comp/detr/coco/r101-dc5
